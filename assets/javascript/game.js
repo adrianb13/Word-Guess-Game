bb921f152bbd4571250e6534c7eaf5ea819a0666
Adrian Brinoes <Coding4Adrian@yahoo.com>
2018-11-30 21:34:00 -0800

    index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,20 +11,36 @@ console.log(lettersToGuess);
 
 document.onkeyup = function(event) {
     var directionsText= document.getElementById("directions-text");
-    directionsText.textContent = "Choose a letter to guess the word."
+        directionsText.textContent = "Choose a letter to guess the word."
     var userText = document.getElementById("user-text");
+        userText.textContent = "You guessed: " + event.key;
 
-var guess = event.key;
-var rightText = document.getElementById("right-text");
+    var guess = event.key;
+    var rightText = document.getElementById("right-text");
+    var wrongText = document.getElementById("wrong-text");
+    var guessLeft = 6
 
-for (j = 0; j < lettersToGuess.length; j++) {
-    if (guess === lettersToGuess[j]) {
-        console.log(guess = lettersToGuess[j]);
-        rightText.textContent = "You guessed CORRECT!";
+    if (lettersToGuess.indexOf(guess) > -1) {
+        console.log(lettersToGuess.indexOf(guess));
+        var rightText = document.getElementById("right-text");
+            rightText.textContent = "You guessed Correct!";
+        var wrongText = document.getElementById("wrong-text");
+            wrongText.textContent = "";
     } else {
-        rightText.textContent = "You guess WRONG!";
+        guessLeft--;
+        console.log(guessLeft)
+        if (guessLeft > 0) {
+        var wrongText = document.getElementById("wrong-text");
+            wrongText.textContent = "Wrong, Try Again!";
+        var rightText = document.getElementById("right-text");
+            rightText.textContent = "";
+        } else {
+            var wrongText = document.getElementById("wrong-text");
+                wrongText.textContent = "Game Over!";
+            var rightText = document.getElementById("right-text");
+                rightText.textContent = "";
+        }
     }
-}
 
-    userText.textContent = "You guessed: " + event.key;
+
 };
