e7a884258b2c7236b536d01c91762d7dcefc2863
Adrian Brinoes <Coding4Adrian@yahoo.com>
2018-12-01 19:07:02 -0800

    index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,53 @@
-const oceanWords = ["meridian", "underwater", "turtle", "trench", "tidalwave", "mackerel", "yellowfin", "sailfish", "octopus", "starfish", "crustaceans", "cephalopods", "invertebrates", "nautilus", "anemone", "jellyfish", "dolphin", "hurricane"];
+const oceanWords = ["meridian", "underwater", "turtle", "trench", "tidalwave", "mackerel", "yellowfin", "sailfish", "octopus", "starfish", "crustaceans", "cephalopods", "invertebrates", "nautilus", "anemone", "jellyfish", "dolphin", "hurricane", "mediterranean"];
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-let gameWord = oceanWords[Math.floor(Math.random() * 18)];
+let gameWord = oceanWords[Math.floor(Math.random() * 19)];
     console.log(gameWord);
 
-let lettersToGuess = []
+let lettersToGuess = [];
+let blankDisplay = [];
     for (i = 0; i < gameWord.length; i++) {
         lettersToGuess.push(gameWord.charAt(i));
+        blankDisplay[i] = "_ ";
+        blankDisplay.push(gameWord.charAt(i));
     }
 console.log(lettersToGuess);
+blankDisplay.splice(lettersToGuess.length ,1);
+blankDisplay.join(" ");
+console.log(blankDisplay.join(" "));
 
 document.onkeyup = function(event) {
-    var directionsText= document.getElementById("directions-text");
-        directionsText.textContent = "Choose a letter to guess the word."
+    var dashes = document.getElementById("display-dashes");
+        dashes.textContent = blankDisplay.join(" ");
+
+    var letters = document.getElementById("letters-left");
+        letters.textContent = "Letters available: " + alphabet.join(" ");
+        
+    var directionsText = document.getElementById("directions-text");
+        directionsText.textContent = "Choose a letter to guess the word.";
+
+
+    var guess = event.key.toLowerCase();
     var userText = document.getElementById("user-text");
         userText.textContent = "You guessed: " + event.key;
 
-    var guess = event.key;
+    
     var rightText = document.getElementById("right-text");
     var wrongText = document.getElementById("wrong-text");
-    var guessLeft = 6
+    var guessLeft = 6;
+
 
     if (lettersToGuess.indexOf(guess) > -1) {
         console.log(lettersToGuess.indexOf(guess));
+        console.log(guessLeft);
+    // Displays chosen letter is correct
         var rightText = document.getElementById("right-text");
             rightText.textContent = "You guessed Correct!";
         var wrongText = document.getElementById("wrong-text");
             wrongText.textContent = "";
     } else {
-        guessLeft--;
-        console.log(guessLeft)
         if (guessLeft > 0) {
+    // Displays chosen letter is wrong
         var wrongText = document.getElementById("wrong-text");
             wrongText.textContent = "Wrong, Try Again!";
         var rightText = document.getElementById("right-text");
@@ -42,5 +60,23 @@ document.onkeyup = function(event) {
         }
     }
 
+    if (lettersToGuess.indexOf(guess) < 0 && guessLeft > 0) {
+        guessLeft = guessLeft - 1;
+        console.log(guessLeft);
+    } 
+
+    // Replaces dashes with correct letters guess
+    for (j = 0; j < lettersToGuess.length; j++) {
+    if (lettersToGuess[j].charAt(0) === guess) {
+        blankDisplay.splice(lettersToGuess.indexOf(guess), 1, event.key);
+        dashes.textContent = blankDisplay.join(" ");
+        }
+    }
+    // Removes letters as letters are press
+    if (alphabet.indexOf(guess) > -1) {
+        alphabet.splice(alphabet.indexOf(guess), 1);
+        var letters = document.getElementById("letters-left");
+        letters.textContent = "Letters available: " + alphabet.join(" ");
+    }
 
 };
